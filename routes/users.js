02b80412abc64195a512a96fb103b87ff7447d9e
Andy Evans <andrewevans0@outlook.com>
2018-03-15 12:42:34 +0000

    reformatted the files

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.send(JSON.stringify({hello: 'hello'}));
-});
+router.get('/', function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	res.send(JSON.stringify({hello: 'hello'}))
+})
 
-module.exports = router;
+module.exports = router
