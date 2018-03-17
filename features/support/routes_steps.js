c13fe74171d7dcdc8323861119a4b0479f750acb
Andy Evans <andrewevans0@outlook.com>
2018-03-17 21:55:09 +0000

    updated cucumber tests

--- features/support/routes_steps.js
+++ features/support/routes_steps.js
@@ -3,18 +3,17 @@ const { Given, When, Then } = require('cucumber')
 const app = require('../../app')
 const request = require('supertest')
 
+let getRequest = ''
+
 Given('a route I want to test {string}', function(string, callback) {
 	callback()
 })
 
 When('I fire it up {string} {int}', function(string, number, callback) {
-	request(app)
-		.get(string)
-		.expect(number, callback)
+	getRequest = request(app).get(string)
+	callback()
 })
 
 Then('I get a working function response {string} {int}', function(string, number, callback) {
-	request(app)
-		.get(string)
-		.expect(number, callback)
+	getRequest.expect(number, callback)
 })
