1568949d737fe7132277213eebc2f95d4aec42c1
Andy Evans <andrewevans0@outlook.com>
2018-04-11 22:26:27 +0100

    adding some test cleanup

--- features/support/routes_steps.js
+++ features/support/routes_steps.js
@@ -1,9 +1,17 @@
 // features/support/steps.js
-const { Given, When, Then } = require('cucumber')
+const { Given, When, Then, Before, After } = require('cucumber')
 const app = require('../../app')
 const request = require('supertest')
+const world = require('./world').world
+let getRequest
 
-let getRequest = ''
+Before(() => {
+	getRequest = null
+})
+
+After(() => {
+	getRequest = null
+})
 
 Given('a route I want to test {string}', function(string, callback) {
 	callback()
@@ -15,5 +23,8 @@ When('I fire it up {string} {int}', function(string, number, callback) {
 })
 
 Then('I get a working function response {string} {int}', function(string, number, callback) {
+	const world = this
+	console.log('what is the test?')
+	console.log(world.variable)
 	getRequest.expect(number, callback)
 })
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,6 @@ router.get('/', function(req, res) {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 	let url_parts = url.parse(req.url, true)
 	let query = url_parts.query
-	console.log('test')
-	console.log(query)
 	if (Object.keys(query).length > 0) {
 		res.status(500).send({ error: 'Something failed!' })
 	}
